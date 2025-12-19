a1e3e0e571dd667aa53b9c8d1b538e5be1fb3fc4
Himaadel2 <himaadel186@gmail.com>
2025-12-19 18:12:35 +0200

    Bouns Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,20 @@
 import { Link } from 'react-router-dom';
 
 
-export default function Navbar() {
+export default function Navbar({ darkMode, setDarkMode }) {
 return (
 <nav>
 <Link to="/">Dashboard</Link> |{' '}
 <Link to="/add-project">Add Project</Link> |{' '}
 <Link to="/add-task">Add Task</Link>
+
+
+<button
+   className="dark-toggle"
+   onClick={() => setDarkMode(!darkMode)}
+>
+   {darkMode ? "☀ Light" : "🌙 Dark"}
+</button>
 </nav>
 );
 }
